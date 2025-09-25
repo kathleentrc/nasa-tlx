b4f82b0910d2405f899d672cb1ce6d66f04a5dae
Kathleen Therese Cruz <137745010+kathleentrc@users.noreply.github.com>
2025-09-25 15:54:49 +0800

    major changes

--- server/public/javascripts/default.js
+++ server/public/javascripts/default.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
 		tableoutput = "",
 		no_score = "–",
 		weighted_tlx = false,
-		total_rounds = 6,
+		total_rounds = 5,
 		current_round = 1,
 		DEBUG = true,
 		settings = {},
@@ -68,7 +68,6 @@ $(document).ready(function() {
 	}
 
 	/* creates an array with a given length and fills it up with a given value */
-
 	function new_filled_array(length, value) {
 		var array = new Array(length);
 		while (--length >= 0) {
@@ -80,7 +79,7 @@ $(document).ready(function() {
 	function update_info() {
 		// console.log("updating info");
 		$(".info").remove();
-		$("<ul class='info'><li><strong>Participant:</strong> " + settings['participant_id'] + "</li><li><strong>Group:</strong> " + settings['experiment_group'] + "</li><li><strong>Round:</strong> " + current_round + "</li></ul>").insertAfter("h2");
+		$("<ul class='info'><li><strong>Participant:</strong> " + settings['participant_id'] + "</li><li><strong>Round:</strong> " + current_round + "</li></ul>").insertAfter("h2");
 	}
 
 	function reset_scales() {
@@ -131,8 +130,8 @@ $(document).ready(function() {
 	}
 
 	/* hide future steps */
+	$(".step_1, .step_2, .step_3, .step_4, .step_5, .step_6, .alert").hide();
 
-	$(".step_1, .step_2, .step_3, .step_4, .step_5, .step_6, .step_open_questions, .alert").hide();
 
 	/* initialize sliders */
 	reset_scales();
@@ -146,75 +145,29 @@ $(document).ready(function() {
 		} else {
 			settings['participant_id'] = 'unspecified';
 		}
-		// settings['starting_task'] = $("input[name='starting_task']:checked").val();
-		settings['experiment_group'] = $("input[name='experiment_group']:checked").val();
 
-		if (enforce_user_input && (settings['participant_id'] == "" || settings['experiment_group'] == null)) {
-		     // do something 
-		     $(".alert").html('ERROR: some variables have not been set!');
-		     $(".alert").show();
+		let participant_id = $("#participant_id").val().trim();
 
-		} else {
-
-			$(".step_0").hide();
-			$(".alert").hide();
-			$(".step_1").show();
-
-		}
-
-		if(DEBUG) {
-	     	console.log(settings);
-	    }
-
-	});
-
-	/* step 1: Demographics */
-
-	$(".step_1 button").click(function() {
-
-		settings['age'] = $('#age').val();
-		settings['gender'] = $("input[name='gender']:checked").val();
-		settings['profession'] = $('#profession').val();
-
-		if (enforce_user_input && (settings['age'] == "" || settings['gender'] == null || settings['profession'] == "")) {
-		     // do something 
-		     $(".alert").html('ERROR: some data has not been provided!');
-		     $(".alert").show();
-
-		} else {
-
-		// prepare steps for step 2
-
+	if (enforce_user_input && (participant_id === "")) {
+		$(".alert").html('ERROR: Participant Name is required!');
+		$(".alert").show();
+	} else {
 		data_object = {
 			"button_clicks": {}, 	// tlx weights
 			"tlx_value": {},		// tlx raw
 			"likert_value": {}		// subjective feedback
 		};
-
-		// reset input values and thrown error paragraphs caused by input submits
-		$(".step_1 input[type='text']").val("");
-		$(".step_1 .cf p").remove();
-
-		// check if proband already completed a task
-		var proband_exists = false,
-			task_exists = false;
-
-		$(".step_1").hide();
+		
+		$(".step_0").hide();
+		$(".alert").hide();
 		$(".step_2").show();
+	}
 
-		}
 
 		if(DEBUG) {
-			console.log(settings);
-		}
-	});
+	     	console.log(settings);
+	    }
 
-	// back button: currently not in use
-	$(".step_1 .go_back a").click(function() {
-		$(".step_1 .cf p").remove();
-		$(".step_1").hide();
-		$(".step_0").show();
-		return false;
 	});
 
 	/* step 2: NASA TLX */
@@ -258,8 +211,8 @@ $(document).ready(function() {
 
 		} else {
 			
-			$(".step_4").show();
-
+			//$(".step_4").show();
+			$(".step_5").show();
 		}
 
 	});
@@ -310,106 +263,57 @@ $(document).ready(function() {
 			
 
 			$(".step_3").hide();
-			$(".step_4").show();
+			$(".step_5").show();
 
 		}
 
 	}); // step 3 button
 
-	/* step 4: subjective feedback */
-
-	$(".step_4 button").live("click", function() {
-		$(".alert").hide();
-
-		// save likert values
-
-		data_object["likert_value"][current_round] = [];
-
-		$(".likert").each(function(i) {
-			data_object["likert_value"][current_round][i] = $(this).slider("option", "value");
-		});
-
-		if(DEBUG) {
-			console.log(data_object);
-		}
-
-		$(".step_4").hide();
-
-		current_round++;
-		if(current_round <= total_rounds) {
-			
-			$(".step_5").show();
-			log_partial_data(settings, data_object);
-
-		} else {
-			$(".step_open_questions").show();
-		}
-	});
-
-	/* step 5: NEXT QUESTIONNAIRE */
-
+	/* NEXT QUESTIONNAIRE */
 	$(".step_5 button").live("click", function() {
 		$(".alert").hide();
 		$(".step_5").hide();
-		$(".step_2").show();
-		update_info();
-		reset_scales();
 
-	}); // step 4 button
+		if (current_round < total_rounds) {
+			current_round++; // ✅ increment round
+			$(".step_2").show();
+			update_info();
+			reset_scales();
+		} else {
+			// ✅ after last round, skip straight to final step
+			$(".step_6").show();
+			$(window).scrollTop(0);
 
-	/* step 5: FINAL QUESTIONNAIRE */
+			server_data = create_data_log(settings, data_object, STATUS_COMPLETE);
 
-	$(".step_open_questions button").live("click", function() {
-		$(".alert").hide();
-		$(".step_open_questions").hide();
-		$(".step_6").show();
-		$(window).scrollTop(0);
+			if(DEBUG) {
+				console.log('Sending data to server:');
+				console.log(server_data);
+			}
 
-		// save final questionnaire
-		data_object['questionnaire'] = {
-			'general_effect': $('#general_effect').val(),
-			'effect_task_performance': $('#effect_task_performance').val(),
-			'effect_stress_levels': $('#effect_stress_levels').val(),
-			'other_task_types': $('#other_task_types').val(),
-			'body_location': $('#body_location').val(),
-			'likes': $('#likes').val(),
-			'dislikes': $('#dislikes').val(),
-			'comments': $('#comments').val(),
-		};
-
-		server_data = create_data_log(settings, data_object, STATUS_COMPLETE);
-
-		if(DEBUG) {
-	  		console.log('Sending data to server:');
-	     	console.log(server_data);
+			$.ajax({
+				type: "POST",
+				url: '/',
+				dataType: 'json',
+				contentType: "application/json",
+				data: JSON.stringify(server_data),
+				success: function (result,status,xhr) {
+					console.log(status);
+					console.log(result);
+					if(result.status == 'ok') {
+						$(".alert").html('<h4>Success:</h4><p>Your data has been saved.</p>');
+						$(".alert").show();
+					} else {
+						$(".alert").html('<h3>There seems to be a problem with the log server!</h3><p>Please manually save the following data objects:</p><h4>Settings:</h4><p>' + JSON.stringify(settings) + '</p><h4>Data</h4>' + JSON.stringify(data_object) + '</p>');
+						$(".alert").show();
+					}
+				},
+				error: function(xhr,status,error) {
+					console.log("Error when transmitting data!"); 
+					$(".alert").html('<h3>There seems to be a problem with the log server!</h3><p>Please manually save the following data objects:</p><h4>Settings:</h4><p>' + JSON.stringify(settings) + '</p><h4>Data</h4>' + JSON.stringify(data_object) + '</p>');
+					$(".alert").show();
+				}
+			});
 		}
-
-		// send final dataset to server
-		$.ajax ({
-	        type: "POST",
-	        url: '/',
-	        dataType: 'json',
-	        contentType: "application/json",
-	        data: JSON.stringify(server_data),
-	        success: function (result,status,xhr) {
-	        	console.log(status);
-	        	console.log(result);
-	        	if(result.status == 'ok') {
-	        		$(".alert").html('<h4>Success:</h4><p>Your data has been saved.</p>');
-	     			$(".alert").show();
-	        	} else {
-	        		$(".alert").html('<h3>There seems to be a problem with the log server!</h3><p>Please manually save the following data objects:</p><h4>Settings:</h4><p>' + JSON.stringify(settings) + '</p><h4>Data</h4>' + JSON.stringify(data_object) + '</p>');
-	     			$(".alert").show();
-	        	}
-	        },
-	        error: function(xhr,status,error) {
-	        	console.log("Error when transmitting data!"); 
-	        	console.log(status);
-	        	console.log(error);
-	        	$(".alert").html('<h3>There seems to be a problem with the log server!</h3><p>Please manually save the following data objects:</p><h4>Settings:</h4><p>' + JSON.stringify(settings) + '</p><h4>Data</h4>' + JSON.stringify(data_object) + '</p>');
-	     		$(".alert").show();
-	        }
-	    });
 	});
-
 });
